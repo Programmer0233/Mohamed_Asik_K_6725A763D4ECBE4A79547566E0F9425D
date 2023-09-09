3dd79c813fa517971621120fd5092e08fb48f62c
Programmer0233 <144516929+Programmer0233@users.noreply.github.com>
2023-09-09 12:43:20 +0530

    Create Challenge2.py

---
+++ Challenge - Uni 1/Challenge2.py
@@ -0,0 +1,15 @@
+def isLeapYear(year):
+  if year % 4 == 0 and year % 100 != 0:
+    return True
+  elif year % 400 == 0:
+    return True
+  else:
+    return False
+
+
+year = int(input("Enter a Year : "))
+
+if isLeapYear(year):
+  print("{} is a leap year.".format(year))
+else:
+  print("{} is not a leap year.".format(year))
